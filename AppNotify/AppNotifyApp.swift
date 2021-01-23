702f7caf5e3037f59096f88729471a283efbb299
张亚飞 <672233580@qq.com>
2021-01-23 22:13:41 +0800

    Initial Commit

---
+++ AppNotify/AppNotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppNotifyApp.swift
+//  AppNotify
+//
+//  Created by 张亚飞 on 2021/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AppNotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
